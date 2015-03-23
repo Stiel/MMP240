34c8dee70da533a8f1179720d8998aaeda4718a7
Stiel <jeremy.t.dubose@gmail.com>
2015-03-23 13:43:18 -0400

    update
    Other class stuff

---
+++ custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    
+    $('.hide-title img').mouseenter(function () {
+        $(this).siblings().animate({
+            "opacity": "1"
+        }, 800)
+    });
+
+    $('.hide-title img').mouseleave(function () {
+        $(this).siblings().animate({
+            "opacity": "0"
+        }, 800);
+    });
+                  
+
+
+});
+
+       
+
+
+
